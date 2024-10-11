d0b671a25783f1021b0bdcc2b3f1d8620a6f029c
Osamak77 <hliwaoussama7@gmail.com>
2024-10-11 09:39:06 +0200

    ok

---
+++ Terzo/src/Terzo.java
@@ -0,0 +1,13 @@
+
+public class Terzo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float l1 = 45.3f;
+		float l2 = 67.2f;
+		double area = (l1 * l2)/2;
+		
+	}
+
+}
---
+++ secondo/src/secondo/secondo.java
@@ -0,0 +1,30 @@
+package secondo;
+
+public class secondo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ciao");
+		System.out.println("come");
+		System.out.println("va");
+		byte b1 = 10;
+		byte b2;
+		b2 = 100;
+		byte b3, b4, b5;
+
+		System.out.println("il valore di b1 è: " + b1);
+		b1 = 127;
+		System.out.println("il valore di b1 è: " + b1);
+		b1 += 1;
+		System.out.println("il valore di b1 è: " + b1);
+		
+		double  d1 =2.3;
+		float f1 = 2.3F;
+		
+		int v1 = 35;
+		int v2 = 41;
+		System.out.println(v1 & v2);
+		
+	}
+
+}
