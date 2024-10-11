6bff67e1715ca8c4fc467d5d0ad8e5fbd6f1e094
Osamak77 <hliwaoussama7@gmail.com>
2024-10-11 13:57:35 +0200

    ok

---
+++ Esercizi/src/Computer.java
@@ -0,0 +1,18 @@
+
+public class Computer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CreateComputer cmp1 = new CreateComputer(100.0 , 80.40 , 30.0 , 20.0 , 30,70)
+		double prezzo ;
+		double peso;
+		double larghezza;
+		double altezza;
+		double profondita;
+		String produttore;
+		double annoProduzione;
+		
+	}
+
+}
