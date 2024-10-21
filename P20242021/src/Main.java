727865fe4602d629ce2df94459ed4ebdfff45b40
Osamak77 <hliwaoussama7@gmail.com>
2024-10-21 13:53:41 +0200

    o

---
+++ P20242021/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i= 1 ; i<=10; i++) {
+			double a = Math.random();
+			System.out.println(i+  ")" + ((i>9)? "" : " ") + a );
+		}
+	
+	}
+
+}
